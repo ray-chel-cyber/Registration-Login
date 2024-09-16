5983bdd0f8a8cc4a1d2da58d7e6a7e564071e768
ray-chel-cyber <nkansahracheal8@gmail.com>
2024-09-16 11:44:14 +0000

    Resgistration login

---
+++ scripts/register.js
@@ -0,0 +1,11 @@
+// Select form by name
+const registerForm = document.getElementById('register-form');
+// Handle form submit event
+registerForm.onsubmit = function registerUser() {
+    //  Collect username
+    // Collect email
+    // Collect password
+    // Save user information
+    // Send confirmation email
+}
+
